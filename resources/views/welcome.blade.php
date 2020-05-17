7978bdd0fd7e220886889e90cf7901f1d8e3b15e
alex <alexxustudy@gmail.com>
2020-05-17 11:31:19 +0800

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,229 @@
-@section("title", "管理员管理")
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>玩转首页</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<div class="container-fluid">
+    <div class="row clearfix">
+        <div class="header">
+            <div class="col-md-12 column distince">
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                    <div class="title">
+                        <span>微赚</span>
+                        <span class="small-title">|ASO智能投放平台</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 column header-content">
+                <div class="col-md-2"></div>
+                <div class="col-md-6">
+                    <span>助力应用</span>
+                    <span style="color: #59F7F0">排名冲榜</span>
+                    <div class="small-title" style="color: #59F7F0">
+                        效果好 风险低 转化高
+                    </div>
+                    <div class="header-btn-margin">
+                        <button type="button" class="btn btn-default header-btn">我要投放</button>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="show-contact">
+                        <div class="contacts">
+                            <span class="people">郭先生</span><br />
+                            <span class="phone">186 1622 3441</span>
+                            <img class="wechat-image" src="images/guozhen.jpg">
+                        </div>
+                        <div class="contacts">
+                            <span class="people">王先生</span><br />
+                            <span class="phone">138 1759 6275</span>
+                            <img class="wechat-image" src="images/guozhen.jpg">
+                        </div>
+                        <div class="contacts">
+                            <span class="people">宋女士</span><br />
+                            <span class="phone">150 2190 6812</span>
+                            <img class="wechat-image" src="images/guozhen.jpg">
+                        </div>
 
-@extends("admin::layouts.admin")
+
+                    </div>
+
+                </div>
+                <div class="col-md-1">
+                    <div class="coopation">
+                        <img class="images" src="images/hezuo.png">
+                        <img class="images" src="images/up.png">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="col-md-12 column" style="text-align: center">
+                <img class="content-images" src="images/services.png">
+            </div>
+            <div class="col-md-12 column" style="margin-top: 30px">
+                <div class="col-md-4 ">
+                    <div class="intro">
+                        <img class="intro-img" src="images/youhua.png" /><br />
+                        <span>
+                            ASO优化
+                        </span>
+                        <div class="intro-detail">
+                            抢占应用搜索,增加曝光及自然流量,增加关键字覆盖量
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="intro">
+                        <img class="intro-img" src="images/fuwu.png" /><br />
+                        <span>
+                            ASM服务
+                        </span>
+                        <div class="intro-detail">
+                            关键词竞价精准触达目标用户,搜索结果置顶,轻松获取用户
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="intro">
+                        <img class="intro-img" src="images/yuanshuju.png" /><br />
+                        <span>
+                            元数据优化
+                        </span>
+                        <div class="intro-detail">
+                            优质的标题,应用描述,ICON等多角度优化应用排名
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="service">
+            <div class="col-md-12 column" style="text-align: center;">
+                <img class="content-images" src="images/fuwuyoushi.png">
+                <div class="advantages">
+                    <div class="col-md-4 " style="margin-left: 100px;">
+                        <div style="float: left">
+                        <img class="ad-images" src="images/liuliang.png" />
+                        </div>
+                        <div style="float: right;margin-top: 100px;">
+                            <span style="float: left">优质流量</span><br />
+                            <span style="float: right;font-size: 10px;color: #9C9C9C"><span style="color: #3285FF">全方位的防作弊</span>大数据监控<br />保证真实用户试玩下载</span>
+
+                        </div>
+                    </div>
+                    <div class="col-md-5 " style="margin-left: 100px;">
+                        <div style="float: left">
+                            <img class="ad-images" src="images/baozhang.png" />
+                        </div>
+                        <div style="float: right;margin-top: 100px;    margin-right: 110px;">
+                            <span style="float: left">效果保障</span><br />
+                            <span style="float: right;font-size: 10px;color: #9C9C9C">高额激励+<span style="color: #3285FF">游戏化产品体验</span><br />提升用户下载量及转化率</span>
+
+                        </div>
+                    </div>
 
 
-@section("breadcrumb")
-    <div class="admin-breadcrumb">
-         <span class="layui-breadcrumb">
-            <a href="{{ route("admin-user.index") }}">滴滴滴滴滴测试</a>
-        </span>
+                    <div class="col-md-4 " style="margin-left: 100px;">
+                        <div style="float: left">
+                            <img class="ad-images" src="images/guibi.png" />
+                        </div>
+                        <div style="float: right;margin-top: 100px;">
+                            <span style="float: left">风险规避</span><br />
+                            <span style="float: right;font-size: 10px;color: #9C9C9C">大数据实时监控<span style="color: #3285FF">规避掉榜风<br />险</span>榜单排名持续领先</span>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4 " style="margin-left: 100px;">
+                        <div style="float: left">
+                            <img class="ad-images" src="images/xingjiabi.png" />
+                        </div>
+                        <div style="float: right;margin-top: 100px;">
+                            <span style="float: left">高性价比</span><br />
+                            <span style="float: right;font-size: 10px;color: #9C9C9C">专业团队对行业关键字筛选<br />相同词<span style="color: #3285FF">花钱少效果好</span></span>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="content">
+            <div class="col-md-12 column" style="text-align: center">
+                <img class="content-images" src="images/yonghufankui.png">
+            </div>
+
+            <div class="col-md-12 column" style="margin-top: 30px">
+                <div class="col-md-4 ">
+                    <div class="intro">
+                        <div style="height: 50px"></div>
+
+                        <span style="color: #2C8FEF">
+                            客户效果好
+                        </span>
+                        <div class="intro-detail">
+                            很专业的SAO推广平台，通过应用商店规则算法优化APP排名比起ASM效果好成本低                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="intro">
+                        <div style="height: 50px"></div>
+
+                        <span style="margin-top: 50px;color: #2C8FEF">
+                            风控机制完善
+                        </span>
+                        <div class="intro-detail">
+                            微赚可以将账号信息、设备信息、用户行为和第三方检测关联起来，完成一个层层过滤的全局防御机制。<br />
+                            全方位检测并识别批量机刷用户和羊毛群，风控机制我们很满意。
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="intro">
+                        <div style="height: 50px"></div>
+
+                        <span style="color: #2C8FEF">
+                            自然流量提高
+                        </span>
+                        <div class="intro-detail">
+                            试玩纪优化应用商店的关键词，让我们该关键排名高很多，在应用商店得到更多曝光，自然流量也增加很多。
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
     </div>
-@endsection
+
+    <div class="row clearfix footer">
+        <div style="margin-top: 50px">
+            公司地址:上海市普陀区中山北路2000号23楼2339室<br />
+            联系方式:18616223441<br />
+            为众多广告主提供ASO优化服务，助理广告主高效获取真实流量<br />
+        </div>
+        <div class="intro-detail">
+            上海威曼巴网络科技有限公司，沪ICP备20002938号
+        </div>
+
+    </div>
+</div>
+
+
+</html>
